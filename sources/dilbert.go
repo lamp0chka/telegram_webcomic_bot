b11d40f8f60c7353677b01ffbdbf73d8b138914c
Marco Melletti <mellotanica@hotmail.it>
2018-04-04 17:21:14 +0200

    enforce dilbert image loading

--- sources/dilbert.go
+++ sources/dilbert.go
@@ -2,14 +2,25 @@ package sources
 
 import (
 	"github.com/mmcdole/gofeed"
+	"github.com/PuerkitoBio/goquery"
 )
 
 func scrapeDilbert(item *gofeed.Item, src feedSrc) ([]comicUpdate, error) {
+	gq, err := goquery.NewDocument(item.Link)
+	if err != nil {
+		return nil, err
+	}
+
+	comic, ok := gq.Find(".img-comic").First().Attr("src")
+	if !ok {
+		comic = item.Link
+	}
+
 	c := make([]comicUpdate, 1)
 	c[0] = comicUpdate{
 				source: src.name,
 				title: item.Title,
-				url: item.Link,
+				url: comic,
 			}
 	return c, nil
 }
